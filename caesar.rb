17735c14ddc0467f0335be54be0a2aa379b6d02d
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 16:28:39 +0100

    bit more expressive variable naming

--- caesar.rb
+++ caesar.rb
@@ -1,8 +1,8 @@
 def caesar (string, left_shift=10)
 # Generate our "ciphers"
-  upcase_letters = ("A".."Z").to_a.rotate!(left_shift)
-  downcase_letters = ("a".."z").to_a.rotate!(left_shift)
-  numerals = ("0".."9").to_a.rotate!(left_shift)
+  upcase_cipher = ("A".."Z").to_a.rotate!(left_shift)
+  downcase_cipher = ("a".."z").to_a.rotate!(left_shift)
+  numeral_cipher = ("0".."9").to_a.rotate!(left_shift)
 # Initialize a string to return
   ciphered_string = ""
 # Make sure the string is UTF-8 encoded
@@ -10,11 +10,11 @@ def caesar (string, left_shift=10)
 # Iterate through string to find and cipher all ciphered characters
   string.each_char do |char|
     if char.ord.between?(97, 122)
-      ciphered_string += downcase_letters[(char.ord-97)]
+      ciphered_string += downcase_cipher[(char.ord-97)]
     elsif char.ord.between?(65, 90)
-      ciphered_string += upcase_letters[(char.ord-65)]
+      ciphered_string += upcase_cipher[(char.ord-65)]
     elsif char.ord.between?(48, 57)
-      ciphered_string += numerals[(char.ord-48)]
+      ciphered_string += numeral_cipher[(char.ord-48)]
 # Keep non-ciphered characters (whitespace, punctuation, etc)
     else
       ciphered_string += char
