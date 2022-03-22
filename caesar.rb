b740589bef8b6e506ed5f13b96c0b2bc8da288cc
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 15:27:40 +0100

    moved shift to alphabet generation

--- caesar.rb
+++ caesar.rb
@@ -1,12 +1,12 @@
 def caesar (string, left_shift)
-  upcase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-  downcase_letters = "abcdefghijklmnopqrstuvwxyz".split("")
+  upcase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").rotate!(left_shift)
+  downcase_letters = "abcdefghijklmnopqrstuvwxyz".split("").rotate!(left_shift)
   ciphered_string = ""
   string.each_char do |char|
-    if char.ord >= 65 && char.ord <= 90
-      ciphered_string += upcase_letters[(char.ord-65)+left_shift]
-    elsif char.ord >= 97 && char.ord <= 122
-      ciphered_string += downcase_letters[(char.ord-97)+left_shift]
+    if char.ord >= 97 && char.ord <= 122
+      ciphered_string += downcase_letters[(char.ord-97)]
+    elsif char.ord >= 65 && char.ord <= 90
+      ciphered_string += upcase_letters[(char.ord-65)]
     else
       ciphered_string += char
     end
@@ -18,6 +18,4 @@ end
 
 #p caesar("Hello world", 10)
 
-downcase_letters = "abcdefghijklmnopqrstuvwxyz".split("")
-p downcase_letters.size
-p downcase_letters[("w".ord-97)+10]
+p downcase_letters = "abcdefghijklmnopqrstuvwxyz".split("").rotate!(-3)
