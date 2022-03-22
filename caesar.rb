72f4cf5d5e9969cd83f59c81d454572abc148b8f
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 16:07:45 +0100

    more elegant range check with #between?

--- caesar.rb
+++ caesar.rb
@@ -7,11 +7,11 @@ def caesar (string, left_shift=10)
   ciphered_string = ""
   string.each_char do |char|
 # Check for whether the current character needs to be ciphered and cipher it if so
-    if char.ord >= 97 && char.ord <= 122
+    if char.ord.between?(97, 122)
       ciphered_string += downcase_letters[(char.ord-97)]
-    elsif char.ord >= 65 && char.ord <= 90
+    elsif char.ord.between?(65, 90)
       ciphered_string += upcase_letters[(char.ord-65)]
-    elsif char.ord >= 48 && char.ord <= 57
+    elsif char.ord.between?(48, 57)
       ciphered_string += numerals[(char.ord-48)]
 # Keep non-ciphered characters (whitespace, punctuation, etc)
     else
