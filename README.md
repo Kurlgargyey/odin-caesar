e880544766400ddf5eeaa75a297d8c7b652cb031
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 15:48:25 +0100

    added some better comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # odin-caesar
-Simple caesar cipher implementation
+Simple caesar cipher implementation for The Odin Project
--- caesar.rb
+++ caesar.rb
@@ -1,15 +1,19 @@
 def caesar (string, left_shift=10)
+# Generate our "ciphers"
   upcase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").rotate!(left_shift)
   downcase_letters = "abcdefghijklmnopqrstuvwxyz".split("").rotate!(left_shift)
   numerals = "0123456789".split("").rotate!(left_shift)
+# Initialize a string to return
   ciphered_string = ""
   string.each_char do |char|
+# Check for whether the current character needs to be ciphered and cipher it if so
     if char.ord >= 97 && char.ord <= 122
       ciphered_string += downcase_letters[(char.ord-97)]
     elsif char.ord >= 65 && char.ord <= 90
       ciphered_string += upcase_letters[(char.ord-65)]
     elsif char.ord >= 48 && char.ord <= 57
       ciphered_string += numerals[(char.ord-48)]
+# Keep non-ciphered characters (whitespace, punctuation, etc)
     else
       ciphered_string += char
     end
