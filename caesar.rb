2f5e731ef8714f8f7653fe3d1d950a8c388ab7e6
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-28 14:52:05 +0200

    refactored with smaller methods

--- caesar.rb
+++ caesar.rb
@@ -1,24 +1,36 @@
-def caesar(string, left_shift=10)
-  # Generate our "ciphers"
-  upcase_cipher = ('A'..'Z').to_a.rotate!(left_shift)
-  downcase_cipher = ('a'..'z').to_a.rotate!(left_shift)
-  numeral_cipher = ('0'..'9').to_a.rotate!(left_shift)
+# frozen_string_literal: true
+
+def generate_ciphers(shift)
+  # Generate our ciphers
+  ciphers = {}
+  ciphers[:downcase_cipher] = ('a'..'z').to_a.rotate!(shift)
+  ciphers[:upcase_cipher] = ('A'..'Z').to_a.rotate!(shift)
+  ciphers[:numeral_cipher] = ('0'..'9').to_a.rotate!(shift)
+  ciphers
+end
+
+def cipher_char(char, ciphers)
+  if char.ord.between?(97, 122)
+    ciphers[:downcase_cipher][(char.ord - 97)]
+  elsif char.ord.between?(65, 90)
+    ciphers[:upcase_cipher][(char.ord - 65)]
+  elsif char.ord.between?(48, 57)
+    ciphers[:numeral_cipher][(char.ord - 48)]
+  else
+    # Keep non-ciphered characters (whitespace, punctuation, etc)
+    char
+  end
+end
+
+def caesar(string, shift)
+  ciphers = generate_ciphers(shift)
   # Initialize a string to return
-  ciphered_string = ''
+  ciphered_string = +''
   # Make sure the string is UTF-8 encoded
   string = string.encode('UTF-8', undef: :replace, invalid: :replace, replace: '')
   # Iterate through string to find and cipher all ciphered characters
   string.each_char do |char|
-    if char.ord.between?(97, 122)
-      ciphered_string += downcase_cipher[(char.ord-97)]
-    elsif char.ord.between?(65, 90)
-      ciphered_string += upcase_cipher[(char.ord-65)]
-    elsif char.ord.between?(48, 57)
-      ciphered_string += numeral_cipher[(char.ord-48)]
-    else
-      # Keep non-ciphered characters (whitespace, punctuation, etc)
-      ciphered_string += char
-    end
+    ciphered_string += cipher_char(char, ciphers)
   end
   ciphered_string
 end
